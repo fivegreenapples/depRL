d0ff7010e80c2e25a7c8f10cae7b4f25474f3d93
Ben Holland <ben@photoshelter.com>
2026-04-14 09:02:06 +0100

    Add support for loading an array of checkpoints via "all" or a comma separated list of ids.

--- deprl/utils/load_utils.py
+++ deprl/utils/load_utils.py
@@ -67,16 +67,43 @@ def load_checkpoint(checkpoint_path, checkpoint="last"):
         if file[:5] == "step_":
             checkpoint_id = file.split(".")[0]
             checkpoint_ids.append(int(checkpoint_id[5:]))
+    checkpoint_ids.sort()
 
     if checkpoint_ids:
         # Use the last checkpoint.
         if checkpoint == "last":
-            checkpoint_id = max(checkpoint_ids)
+            checkpoint_id = checkpoint_ids[-1]
             checkpoint_path = os.path.join(
                 checkpoint_path, f"step_{checkpoint_id}"
             )
 
-        # Use the specified checkpoint.
+        # Use all checkpoints.
+        elif checkpoint == "all":
+            checkpoint_path = [
+                os.path.join(checkpoint_path, f"step_{checkpoint_id}")
+                for checkpoint_id in checkpoint_ids
+            ]
+
+        # Comma-separated list of checkpoint ids.
+        elif "," in checkpoint:
+            checkpoints = [
+                int(chkpt.strip())
+                for chkpt in checkpoint.split(",")
+                if chkpt.strip() != ""
+            ]
+            for checkpoint_id in checkpoints:
+                if checkpoint_id not in checkpoint_ids:
+                    logger.error(
+                        f"Checkpoint {checkpoint_id} "
+                        f"not found in {checkpoint_path}"
+                    )
+            checkpoint_path = [
+                os.path.join(checkpoint_path, f"step_{checkpoint_id}")
+                for checkpoint_id in checkpoints
+                if checkpoint_id in checkpoint_ids
+            ]
+
+        # Use the specified checkpoint
         else:
             checkpoint_id = int(checkpoint)
             if checkpoint_id in checkpoint_ids:
