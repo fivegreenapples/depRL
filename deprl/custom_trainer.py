b9495c31a04b7ffe3e7bafb4b2c1a5980f4af982
Ben Holland <ben@photoshelter.com>
2026-04-22 23:14:26 +0100

    Add support for an adaptive  training curriculum that changes the target velocity based on how high the current running average score is.

--- deprl/custom_trainer.py
+++ deprl/custom_trainer.py
@@ -26,6 +26,7 @@ class Trainer:
         test_episodes=20,
         show_progress=True,
         replace_checkpoint=False,
+        curriculum=None,
     ):
         assert epoch_steps <= save_steps
         self.max_steps = int(steps)
@@ -34,6 +35,7 @@ class Trainer:
         self.test_episodes = test_episodes
         self.show_progress = show_progress
         self.replace_checkpoint = replace_checkpoint
+        self.cur = curriculum
 
     def initialize(
         self, agent, environment, test_environment=None, full_save=False
@@ -42,6 +44,14 @@ class Trainer:
         self.agent = agent
         self.environment = environment
         self.test_environment = test_environment
+        if self.cur:
+            self.cur["avg_score"] = 0
+            if self.cur["type"] == "stair":
+                self.cur["target_v"] = self.cur["v_min"]
+            elif self.cur["type"] == "random":
+                self.cur["target_v"] = self.cur["v_min"] + np.random.rand() * (
+                    self.cur["v_max"] - self.cur["v_min"]
+                )
 
     def run(self, params, steps=0, epochs=0, episodes=0):
         """Runs the main training loop."""
@@ -74,6 +84,22 @@ class Trainer:
             logger.store("train/action", actions, stats=True)
 
             # Take a step in the environments.
+            if self.cur:
+                # if we have a curriculum, fudge the target velocity into the actions.
+                # this gets extracted by the environment. can't set this as a kwarg
+                # as the various gymnasium wrappers don't accept arbitrary kwargs.
+                actions = np.hstack(
+                    [
+                        actions,
+                        np.full((actions.shape[0], 1), self.cur["target_v"]),
+                    ]
+                )
+                logger.store(
+                    "train/curriculum/target_vel", self.cur["target_v"]
+                )
+                logger.store(
+                    "train/curriculum/avg_score", self.cur["avg_score"]
+                )
             observations, muscle_states, info = self.environment.step(actions)
             if "env_infos" in info:
                 info.pop("env_infos")
@@ -92,12 +118,20 @@ class Trainer:
                 )
 
             # Check the finished episodes.
+            did_reset = False
             for i in range(num_workers):
                 if info["resets"][i]:
+                    did_reset = True
+                    # print(f"Trainer reset {i} with score {scores[i]}")
                     logger.store("train/episode_score", scores[i], stats=True)
                     logger.store(
                         "train/episode_length", lengths[i], stats=True
                     )
+                    if self.cur:
+                        self.cur["avg_score"] *= self.cur["alpha"]
+                        self.cur["avg_score"] += scores[i] * (
+                            1 - self.cur["alpha"]
+                        )
                     if i == 0:
                         # adaptive energy cost
                         if hasattr(self.agent.replay, "action_cost"):
@@ -109,6 +143,21 @@ class Trainer:
                     scores[i] = 0
                     lengths[i] = 0
                     episodes += 1
+            if did_reset:
+                if self.cur["avg_score"] >= self.cur["threshold"]:
+                    self.cur["avg_score"] = 0
+
+                    if self.cur["type"] == "stair":
+                        self.cur["target_v"] = min(
+                            self.cur["v_max"],
+                            self.cur["target_v"] + self.cur["v_inc"],
+                        )
+                    elif self.cur["type"] == "random":
+                        self.cur["target_v"] = self.cur[
+                            "v_min"
+                        ] + np.random.rand() * (
+                            self.cur["v_max"] - self.cur["v_min"]
+                        )
 
             # End of the epoch.
             if epoch_steps >= self.epoch_steps:
